a7fa0914e81407cbabbac476c574f4fb6f70f6db
Filippo Dembech <filippo.dembech@gmail.com>
2025-10-07 20:58:40 +0200

    pwa: icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+    plugins: [react(), VitePWA({
+      registerType: 'autoUpdate', // automatically updates service worker
+      manifest: {
+        name: 'Kanji Area',
+        short_name: 'Kanji Area',
+        description: 'Practice strike order and handwriting on this area.',
+        start_url: '/kanji-area/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/kanji.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/kanji.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })],
   base: "/kanji-area/",
-  plugins: [react()],
   server: {
     open: true
   }
